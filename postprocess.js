69ed1d179db9bbc0b2b01312ae9c1bb86fd42284
Aron Ambrosiani <aron@ambrosiani.se>
2022-07-22 16:47:45 +0200

    stats

--- postprocess.js
+++ postprocess.js
@@ -9,7 +9,10 @@ import { slugify } from 'https://deno.land/x/slugify/mod.ts'
 // let data = xml2js(text, { compact: true })
 // await writeJSON('arkdes.json', data, null, 2)
 
-var institutions = await readJSON('se_config.json');
+var institutions = await readJSON('se_config.json')
+
+var stats
+
 for (const institution of institutions) {
     let url = 'https://tools.wmflabs.org//glamtools/glamorous.php?doit=1&category=' + institution.cat.replace(' ', '+') + '&use_globalusage=1&ns0=1&depth=9&projects[wikipedia]=1&projects[wikimedia]=1&projects[wikisource]=1&projects[wikibooks]=1&projects[wikiquote]=1&projects[wiktionary]=1&projects[wikinews]=1&projects[wikivoyage]=1&projects[wikispecies]=1&projects[mediawiki]=1&projects[wikidata]=1&projects[wikiversity]=1&format=xml'
     let response = await fetch(url)
@@ -17,121 +20,14 @@ for (const institution of institutions) {
     let data = xml2js(body, { compact: true })
     await writeTXT('data/' + slugify(institution.name, {lower: true, remove: /[$*_+~.,()'"!\-:@]/g}) + '.xml', body)
     await writeJSON('data/' + slugify(institution.name, {lower: true, remove: /[$*_+~.,()'"!\-:@]/g}) + '.json', data, null, 2)
+    stats.push({
+        "name":institution.name, 
+        "cat": institution.cat, 
+        "distinct_used": data.results.stats._attributes.distinct_images,
+        "files": data.results._attributes.images_in_category,
+        "percent_used": data.results.stats._attributes.distinct_images/data.results._attributes.images_in_category,
+        "total_usage": data.results.stats._attributes.total_usage.toFixed(2)
+    })
 }
 
-/*
-
-
-
-
-def process_data(xmlblob):
-    row = {}
-    tree = etree.fromstring(xmlblob)
-    no_files = tree.get("images_in_category")
-    row["files"] = no_files
-    for child_of_root in tree:
-        distinct_used = child_of_root.get("distinct_images")
-        row["percent_used"] = calculate_percent(distinct_used, no_files)
-        row["total_usage"] = child_of_root.get("total_usage")
-        row["distinct_used"] = distinct_used
-    return row
-
-
-def analyze_single(item):
-    url = make_url(item["cat"])
-    data = download_data(url)
-    row = process_data(data)
-    row["name"] = item["name"]
-    row["cat"] = item["cat"].replace(" ", "_")
-    return row
-
-
-def download_data(url):
-    return requests.get(url).text
-
-
-def read_json_file(fname):
-    return json.load(open(fname))
-
-
-def save_file(fname, txt):
-    with open(fname, 'w') as f:
-        f.write(txt)
-        print("Saved file: {}".format(fname))
-
-
-def save_json_file(fname, data):
-    with open(fname, 'w') as f:
-        json.dump(data, f,
-                  ensure_ascii=False,
-                  sort_keys=True, indent=4)
-        print("Saved file: {}".format(fname))
-
-
-def make_wikitable(data):
-    txt = '{| class="wikitable sortable"\n|-\n'
-    txt += "! {}\n".format("institution")
-    txt += "! {}\n".format("files")
-    txt += "! {}\n".format("used")
-    txt += "! {}\n".format("% used")
-    for r in data:
-        link_base = "[[:commons:Category:{}|{}]]"
-        link = link_base.format(r["cat"], r["name"])
-        txt += "|-\n"
-        txt += "| {}\n".format(link)
-        txt += "| {}\n".format(r["files"])
-        txt += "| {}\n".format(r["distinct_used"])
-        txt += "| {}\n".format(r["percent_used"])
-    txt += "|}\n"
-    return txt
-
-
-def make_markdown(data):
-    txt = "| {} | {} | {} | {} |\n".format("institution",
-                                           "files",
-                                           "used",
-                                           "% used")
-    txt += "|---|---|---|---|\n"
-    for r in data:
-        link_base = "https://commons.wikimedia.org/wiki/Category:{}"
-        link_str = link_base.format(r["cat"])
-        link = "[{}]({})".format(r["name"], link_str)
-        txt += "| {} | {} | {} | {} |\n".format(link,
-                                                r["files"],
-                                                r["distinct_used"],
-                                                r["percent_used"])
-    return txt
-
-
-def extract_number_files(json):
-    return int(json["files"])
-
-
-def save_processed(rows):
-    fname = generate_filename()
-    save_json_file("{}.json".format(fname), rows)
-    save_file("{}.md".format(fname), make_markdown(rows))
-    save_file("{}.wiki".format(fname), make_wikitable(rows))
-
-
-def main(f_config):
-    cats = read_json_file(f_config)
-    message = "[{}] I'm going to analyze {} cats...🐈"
-    print(message.format(f_config, len(cats)))
-    rows = []
-    for cat in cats:
-        rows.append(analyze_single(cat))
-    rows.sort(key=extract_number_files, reverse=True)
-    save_processed(rows)
-
-
-if __name__ == "__main__":
-    arguments = {}
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--config",
-                        help='file with categories to analyze',
-                        default="se_config.json")
-    args = parser.parse_args()
-    main(args.config)
-
-*/
+await writeJSON('data/stats.json', stats, null, 2)
